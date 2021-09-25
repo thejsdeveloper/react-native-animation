a2c8c95d96766305db3cd034d10e3b595a536979
vikas yadav <thejsdeveloper@gmail.com>
2021-09-26 00:17:42 +0530

    adding function to restore the navigation state when in DEV mode

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,28 +2,65 @@ import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
+  InitialState,
 } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from "expo-status-bar";
 import * as React from "react";
+import AppLoading from "expo-app-loading";
+
 import { ColorSchemeName } from "react-native";
 import { Routes } from "./Routes";
 
-import LinkingConfiguration from "./LinkingConfiguration";
 import PanGesture from "../screens/PanGesture";
 import HomeScreen from "../screens/HomeScreen";
 
 const Stack = createNativeStackNavigator<Routes>();
+
+const NAVIGATION_KEY = "NAVIGATION_KEY";
 
 export default function Navigation({
   colorScheme,
 }: {
   colorScheme: ColorSchemeName;
 }) {
+  const [isNavigationReady, setIsNavigationReady] = React.useState<boolean>(
+    !__DEV__
+  );
+
+  const [initialState, setInitialState] = React.useState<
+    InitialState | undefined
+  >();
+
+  const onStateChange = React.useCallback(
+    (state) => AsyncStorage.setItem(NAVIGATION_KEY, JSON.stringify(state)),
+    []
+  );
+
+  React.useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const storedState = await AsyncStorage.getItem(NAVIGATION_KEY);
+        const navState = storedState ? JSON.parse(storedState) : undefined;
+        setInitialState(navState);
+      } finally {
+        setIsNavigationReady(true);
+      }
+    };
+
+    if (!isNavigationReady) {
+      restoreState();
+    }
+  }, [isNavigationReady]);
+
+  if (!isNavigationReady) {
+    return <AppLoading />;
+  }
   return (
     <NavigationContainer
-      linking={LinkingConfiguration}
       theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+      {...{ initialState, onStateChange }}
     >
       <StatusBar style="dark" />
       <Stack.Navigator initialRouteName="Home">
