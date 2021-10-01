22fdc8e96aa5478b44c92c617dd4256f654f5b10
vikas yadav <thejsdeveloper@gmail.com>
2021-10-01 22:31:44 +0530

    adding go back functionality

--- src/screens/Theming/components/Header.tsx
+++ src/screens/Theming/components/Header.tsx
@@ -1,6 +1,6 @@
 import { AntDesign } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import Animated, {
   interpolateColor,
   useAnimatedStyle,
@@ -8,8 +8,13 @@ import Animated, {
 import StyleGuide from "../../../components/StyleGuide";
 import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Routes } from "../../../navigation/Routes";
+import { useNavigation } from "@react-navigation/core";
 
 const Header = () => {
+  const { goBack } =
+    useNavigation<NativeStackNavigationProp<Routes, "Theming">>();
   return (
     <SafeAreaView style={[styles.container]}>
       <LinearGradient
@@ -25,7 +30,13 @@ const Header = () => {
         style={[StyleSheet.absoluteFill, { justifyContent: "flex-end" }]}
       >
         <Animated.View style={[styles.container]}>
-          <AntDesign name="arrowleft" size={30} color="white" />
+          <Pressable
+            onPress={() => {
+              goBack();
+            }}
+          >
+            <AntDesign name="arrowleft" size={30} color="white" />
+          </Pressable>
           <Text style={styles.title}>Chat Theme</Text>
         </Animated.View>
       </LinearGradient>
@@ -39,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
-    padding: 15,
+    padding: 10,
   },
   title: {
     ...StyleGuide.typography.title,
