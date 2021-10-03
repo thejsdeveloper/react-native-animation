2a7040ca41adecb0889889e7d8ec6721a46cff61
vikas yadav <thejsdeveloper@gmail.com>
2021-10-03 10:37:09 +0530

    adding swipe surfing component

---
+++ src/screens/SwipeSurfing/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SwipeSurfing = () => {
+  return (
+    <View>
+      <Text>Swipe Surfing</Text>
+    </View>
+  );
+};
+
+export default SwipeSurfing;
+
+const styles = StyleSheet.create({});
