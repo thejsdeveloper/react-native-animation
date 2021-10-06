a697384551e1160f1c765b2a4d472d4a78fb2c6a
vikas yadav <thejsdeveloper@gmail.com>
2021-10-06 14:02:44 +0530

    adding opacity value

--- src/screens/SwipeSurfing/WatchPage.tsx
+++ src/screens/SwipeSurfing/WatchPage.tsx
@@ -4,6 +4,7 @@ import Animated, {
   Extrapolate,
   interpolate,
   useAnimatedStyle,
+  withTiming,
 } from "react-native-reanimated";
 import { toRad } from "react-native-redash";
 import StyleGuide from "../../components/StyleGuide";
@@ -18,22 +19,43 @@ type WatchPageProps = {
 };
 
 const WatchPage = ({ watch, translateX, index }: WatchPageProps) => {
+  const inputRange = [
+    (index - 1) * SCREEEN_WIDTH,
+    index * SCREEEN_WIDTH,
+    (index + 1) * SCREEEN_WIDTH,
+  ];
+
   const rImageStyle = useAnimatedStyle(() => {
     const progress = interpolate(
       translateX.value,
-      [
-        (index - 1) * SCREEEN_WIDTH,
-        index * SCREEEN_WIDTH,
-        (index + 1) * SCREEEN_WIDTH,
-      ],
-      [0, 0, 1],
+      inputRange,
+      [1, 0, 1],
       Extrapolate.CLAMP
     );
 
+    const translateY = interpolate(
+      translateX.value,
+      inputRange,
+      [100, 0, 0],
+      Extrapolate.CLAMP
+    );
+
+    const opacity = interpolate(
+      translateX.value,
+      inputRange,
+      [0.5, 1, 0.5],
+      Extrapolate.CLAMP
+    );
+
+    const angle = `${progress * Math.PI}rad`;
     return {
+      opacity,
       transform: [
         {
-          rotate: `${progress * Math.PI}rad`,
+          rotate: angle,
+        },
+        {
+          translateY: translateY,
         },
       ],
     };
