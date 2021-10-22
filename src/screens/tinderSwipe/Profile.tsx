ce2a836c3e5269b556a0a3a2b4f07d6a9efb1e43
vikas yadav <thejsdeveloper@gmail.com>
2021-10-22 22:42:11 +0530

    profile like and nope button changes

--- src/screens/tinderSwipe/Profile.tsx
+++ src/screens/tinderSwipe/Profile.tsx
@@ -17,10 +17,19 @@ const Profile = ({ profile, onTop }: ProfileProps) => {
   return (
     <View style={[StyleSheet.absoluteFill]}>
       <Image style={styles.image} source={profile.profile} />
-      {/* <View style={styles.overlay}>
-        <View style={styles.header}></View>
-        <View style={styles.footer}></View>
-      </View> */}
+      <View style={styles.overlay}>
+        <View style={styles.header}>
+          <View style={[styles.like]}>
+            <Text style={styles.likeLabel}>LIKE</Text>
+          </View>
+          <View style={[styles.nope]}>
+            <Text style={styles.nopeLabel}>NOPE</Text>
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.name}>{profile.name}</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -34,7 +43,42 @@ const styles = StyleSheet.create({
     height: undefined,
     borderRadius: 8,
   },
-  overlay: {},
-  header: {},
-  footer: {},
+  overlay: {
+    flex: 1,
+    justifyContent: "space-between",
+    padding: 16,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  footer: {
+    flexDirection: "row",
+  },
+  like: {
+    borderWidth: 4,
+    borderRadius: 5,
+    padding: 8,
+    borderColor: "#6ee3b4",
+  },
+  likeLabel: {
+    fontSize: 32,
+    color: "#6ee3b4",
+    fontWeight: "bold",
+  },
+  nope: {
+    borderWidth: 4,
+    borderRadius: 5,
+    padding: 8,
+    borderColor: "#ec5288",
+  },
+  nopeLabel: {
+    fontSize: 32,
+    color: "#ec5288",
+    fontWeight: "bold",
+  },
+  name: {
+    color: "white",
+    fontSize: 32,
+  },
 });
